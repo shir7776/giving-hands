6ee73b271dccb23a0046426f1da68b72d41d5dcb
shir-yehuda <82862990+shir-yehuda@users.noreply.github.com>
2021-08-05 10:53:34 +0300

    refactoring

---
+++ src/components/blog/post.js
@@ -0,0 +1,38 @@
+import ShowMore from "react-show-more";
+import React from "react";
+
+export const Post=({post})=>{
+    return (
+        <li key={post.id} className='ticket'>
+
+            <h5 className='title'>{post.title}</h5>
+            <p className='content'><ShowMore>{post.content}</ShowMore></p>
+
+            <footer>
+                <table>
+
+                    <td>
+                        <div
+                            className='meta-data'>By {post.userEmail} | {new Date(post.creationTime).toLocaleString()}</div>
+                    </td>
+                    <td>
+                        <div className="flexWrap">
+                            {post.labels ?
+                                post.labels.map((lable) => (<div className="lableWrapper">
+                                    <div className="flexCol">{lable}</div>
+                                </div>)) :
+                                null}
+                        </div>
+                    </td>
+                    <td>
+                        <form className="sendMail">
+                            <textarea id="styled"
+                                      placeholder={"send a response to " + post.userEmail.split("@")[0] + " via email"}
+                            ></textarea>
+                            <button>send</button>
+                        </form>
+                    </td>
+                </table>
+            </footer>
+        </li>)
+}
--- src/routs/blog/blogs.js
+++ src/routs/blog/blogs.js
@@ -4,6 +4,7 @@ import styles from './blog.css';
 import ShowMore from 'react-show-more';
 
 import {NewPost} from "../../components/blog/newPost";
+import {Post} from "../../components/blog/post";
 
 export const Blog = () => {
     const [addPost, setAddPost] = useState(false);
@@ -12,45 +13,6 @@ export const Blog = () => {
     const [userEmail, setUserEmail] = useState("");
     const [labels, setLabels] = useState(null);
 
-// const NewPost = ()=>{
-//     return (
-//         <div >
-//             <div className="login-box">
-//                 <h2>New Post</h2>
-//                 <form >
-//                     <div className="user-box" >
-//                         <input type="text" name="title" id="title"  onChange={(e)=>(setTitle(e.target.value))}required/>
-//                         <label>Title*</label>
-//                     </div>
-//                     <div className="user-box">
-//
-//                         <input type="text" name="" required={true} onChange={(e)=>(setContent(e.target.value))}/>
-//                         <label>What Do Yo Want To Write About?*</label>
-//                     </div>
-//                     <div className="user-box">
-//
-//                         <input type="text" name="" required={true} onChange={(e)=>(setUserEmail(e.target.value))}/>
-//                         <label>Your Email*</label>
-//                     </div>
-//                     <div className="user-box">
-//
-//                         <input type="text" name="" required={true} onChange={(e)=>(setLabels(e.target.value))}/>
-//                         <label>Labels You Want To Add?</label>
-//                         <h5 style={{margin:-10}}>*required fields</h5>
-//                     </div>
-//                     <a >
-//                         <span></span>
-//                         <span></span>
-//                         <span></span>
-//                         <span></span>
-//                         Add Ticket
-//                     </a><a onClick={newPostVisibility}>cancle</a>
-//                 </form>
-//             </div>
-//         </div>
-//     )
-// }
-
     const newPostVisibility = () => {
         setAddPost(!addPost);
     };
@@ -58,38 +20,9 @@ export const Blog = () => {
 
         return (<div className={styles.blog}>
             <ul className='tickets'>
-                {posts.map((post, index) => (<li key={post.id} className='ticket'>
-
-                    <h5 className='title'>{post.title}</h5>
-                    <p className='content'><ShowMore>{post.content}</ShowMore></p>
-
-                    <footer>
-                        <table>
-
-                            <td>
-                                <div
-                                    className='meta-data'>By {post.userEmail} | {new Date(post.creationTime).toLocaleString()}</div>
-                            </td>
-                            <td>
-                                <div className="flexWrap">
-                                    {post.labels ?
-                                        post.labels.map((lable) => (<div className="lableWrapper">
-                                            <div className="flexCol">{lable}</div>
-                                        </div>)) :
-                                        null}
-                                </div>
-                            </td>
-                            <td>
-                                <form className="sendMail">
-                            <textarea id="styled"
-                                      placeholder={"send a response to " + post.userEmail.split("@")[0] + " via email"}
-                            ></textarea>
-                                    <button>send</button>
-                                </form>
-                            </td>
-                        </table>
-                    </footer>
-                </li>))}
+                {posts.map((post, index) => (
+                    <Post post={post}/>
+                ))}
             </ul>
         </div>);
     };
@@ -98,7 +31,7 @@ export const Blog = () => {
     const posts = [
         {
             "id": "b7151edd-06bf-5a26-b338-60bf851661ae",
-            "title": "Help I am a beginner in javascript!",
+            "title": "Help I am a beginner in javascript!!!",
             "content": "Hi i need help please! i'm a beginner at java script. i'm trying to use an onlick event to change the color of a button in my code, this button links to and other HTML file. Here is the HTML and CSS code.\n\n\nHTML code:\n\n<a  href=\"index.html\"><button  class=\"btn-hm\">HOME</button></a> \n\n<a href=\"Photogallery.html\"><button class=\"btn-gp\">Photo Gallery</button></a>\n\n\nCSS code: \n\n.btn-hm{\n\nfont-family: 'Mansalva', cursive;\ncolor:white;\ntext-shadow: -2px 0 black, 0 2px black, 2px 0 black, 0 -2px black;\nborder: 4px solid white;\npadding:10px 22px;\nfont-size: 35px;\nborder-radius: 10%;\nbackground-color:gray;\ncursor: pointer;\ncursor: hand;   \n\n}\n\n.btn-gp{\n  padding:10px 22px;\n  font-size: 35px;\n border-radius: 10%;\n  font-family: 'Mansalva', cursive;\n  border: 4px solid white;\n  color: white;\ntext-shadow: -2px 0 black, 0 2px black, 2px 0 black, 0 -2px black;\nbackground-color:gray;\ncursor: pointer;\n    cursor: hand;\n  }\n\nI've tried a few things in JS and it fail every time. please someone help me and explain what i need to doe  :(",
             "userEmail": "wogel@foter.bn",
             "creationTime": 1545625643857

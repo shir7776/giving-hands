6db8d043831cf5b604a0f509dea2118ff044fa5a
shir-yehuda <82862990+shir-yehuda@users.noreply.github.com>
2021-08-05 10:47:50 +0300

    refactoring

---
+++ src/components/blog/newPost.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+export const NewPost = ({setTitle,setContent,setUserEmail,setLabels,newPostVisibility})=>{
+    return (
+        <div >
+            <div className="login-box">
+                <h2>New Post</h2>
+                <form >
+                    <div className="user-box" >
+                        <input type="text" name="title" id="title"  onChange={(e)=>(setTitle(e.target.value))}required/>
+                        <label>Title*</label>
+                    </div>
+                    <div className="user-box">
+
+                        <input type="text" name="" required={true} onChange={(e)=>(setContent(e.target.value))}/>
+                        <label>What Do Yo Want To Write About?*</label>
+                    </div>
+                    <div className="user-box">
+
+                        <input type="text" name="" required={true} onChange={(e)=>(setUserEmail(e.target.value))}/>
+                        <label>Your Email*</label>
+                    </div>
+                    <div className="user-box">
+
+                        <input type="text" name="" required={true} onChange={(e)=>(setLabels(e.target.value))}/>
+                        <label>Labels You Want To Add?</label>
+                        <h5 style={{margin:-10}}>*required fields</h5>
+                    </div>
+                    <a >
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        Add Ticket
+                    </a><a onClick={newPostVisibility}>cancle</a>
+                </form>
+            </div>
+        </div>
+    )
+}

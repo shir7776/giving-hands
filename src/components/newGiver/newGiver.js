71cfd3b03488e432707497721e0987ea65b08d29
shir-yehuda <82862990+shir-yehuda@users.noreply.github.com>
2021-08-08 20:07:54 +0300

    refactored givers management, added new giver though i dont think its needed

---
+++ src/components/newGiver/newGiver.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+export const NewGiver=(props)=>{
+
+        return (
+            <div >
+                <div >
+                    <h2>New Giver</h2>
+                    <form >
+                        <div className="user-box" >
+                            <input type="text" name="name" id="name"  onChange={(e)=>(props.setName(e.target.value))}required/>
+                            <label>Name*</label>
+                        </div>
+                        <div className="user-box">
+
+                            <input type="text" name="phone" required={true} onChange={(e)=>(props.setPhone(e.target.value))}/>
+                            <label>Phone Number*</label>
+                        </div>
+                        <div className="user-box">
+
+                            <input type="text" name="Email" required={true} onChange={(e)=>(props.setEmail(e.target.value))}/>
+                            <label>Email*</label>
+                        </div>
+                        <div className="user-box">
+                            <h5 style={{margin:-10}}>*required fields</h5>
+                        </div>
+                        <a >
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            Add Giver
+                        </a>
+                    </form>
+                </div>
+            </div>
+        )
+
+}

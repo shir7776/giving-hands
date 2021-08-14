b5d0017dc8a48467541cd0cf7b45b8373da11abd
shir-yehuda <82862990+shir-yehuda@users.noreply.github.com>
2021-08-14 22:10:06 +0300

    added empty blog api

---
+++ src/API/blogAPI.js
@@ -0,0 +1,31 @@
+const getBlog=()=>{
+
+    const lst=[
+        {id:1, name:"rgte",phone:23, email:"sdfgsdfg", area:1},
+        {id:2, name:"rgte",phone:23, email:"sdfgsdfg", area:1},
+        {id:3, name:"rgte",phone:23, email:"sdfgsdfg", area:1},
+        {id:4, name:"rgte",phone:23, email:"sdfgsdfg", area:1},
+        {id:5, name:"rgte",phone:23, email:"sdfgsdfg", area:1},
+        {id:6, name:"rgte",phone:23, email:"sdfgsdfg", area:1},
+        {id:7, name:"rgte",phone:23, email:"sdfgsdfg", area:1}
+
+    ]
+    return lst;
+}
+
+const updateBlog=(giver)=>{
+
+}
+const addBlog=(giver)=>{
+
+}
+const deleteBlog=(giver)=>{
+
+}
+
+export const blogAPI={
+    getBlog,
+    updateBlog,
+    addBlog,
+    deleteBlog
+}

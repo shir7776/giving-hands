852db3fc248c13d4db6c1f4d77f69853789259d0
shir-yehuda <82862990+shir-yehuda@users.noreply.github.com>
2021-08-14 21:38:31 +0300

    added api

--- src/routs/locationMenagement/location_management.js
+++ src/routs/locationMenagement/location_management.js
@@ -1,17 +1,12 @@
 import React, {useState} from "react";
 import MyMapComponent from "../../components/map/map_component";
 import Table from "../../components/table/table_component";
+import {serverAPI} from "../../API/serverAPI";
 
 export const LocationManagement = () => {
     const getLocations = () => {
+        serverAPI.getLocations()
 
-        const lst = [
-            {address: "1", lat: 31, lng: 35, id: 1, area: 1, finished: false},
-            {address: "2", lat: 32, lng: 35, id: 2, area: 2, finished: false},
-            {address: "3", lat: 31, lng: 34, id: 3, area: 3, finished: false},
-            {address: "4", lat: 31.5, lng: 34.5, id: 4, area: 3, finished: false}
-        ]
-        return lst;
     }
     const [locationList, setLocationList] = React.useState(getLocations())
     const [selectedMarker, setSelectedMarker] = useState(false)

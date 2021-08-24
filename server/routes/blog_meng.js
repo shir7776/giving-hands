3ba133bf71697e017d134ec593b14edd35540d59
shir7776 <shir7776@gmail.com>
2021-08-24 13:58:18 +0300

    conection the client to the server in blog

--- server/routes/blog_meng.js
+++ server/routes/blog_meng.js
@@ -9,36 +9,30 @@ var url ="mongodb+srv://hodayara:hodayara@giving-hands.cztzd.mongodb.net/helpHen
 
 router.post('/addNewBlog',async function(req, res,next) {
     try{
+      console.log("in adding new post")
         var blog=[];
-        blog[0]= req.body.id_blog;
-        blog[1]= req.body.title;
-        blog[2] = req.body.content;
-        blog[3] = req.body.email;
-   MongoClient.connect(url, function(err, db) {
-       if (err) throw err;
-       var dbo = db.db("helpHend");
-       var myquery = {id_blog:req.body.id_blog};
-       dbo.collection("blogs").find({myquery}).toArray(function(err, result) {
-        if (err) throw err;
-        if(result.length!=0){
-            setTimeout(() => { res.json({
-                status: 'failed',
-                data:'false',
-                message:"This id_blog is already exist"
-              }); }, 1000);
-      }
-      else{
-        Blog.CREATE(blog);
-        console.log('blog created:' + blog);
+        blog[0]= req.body.title;
+        blog[1] = req.body.content;
+        blog[2] = req.body.userEmail;
+        blog[3]=req.body.creationTime;
+        console.log(blog);
+        Blog.CREATE(blog, (err, item) => {
+          if (err) {
+            console.log(err);
+        }
+        else {
+          console.log('blog created:' + blog);
+            
+        }
+      });
+        
         setTimeout(() => { res.json({
           status: 'success',
           data:'true',
           message:"success"
-        }); }, 1000);   
-     }
-     db.close();
-     });
-    });
+        }); }, 1000); 
+        
+    
    }
    catch{
        res.status(500).send();

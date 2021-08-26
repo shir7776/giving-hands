9d6e3bde47e5cec150c163cf1b355398006ac0e0
shir7776 <shir7776@gmail.com>
2021-08-26 11:08:44 +0300

    refactor

--- server/routes/user_meng.js
+++ server/routes/user_meng.js
@@ -5,8 +5,8 @@ const bcrypt=require('bcrypt');
 
 var MongoClient = require('mongodb').MongoClient;
 //var url = "mongodb+srv://hodayara:hodayara@giving-hands.e9nsj.mongodb.net/helpHend";
-var url ="mongodb+srv://hodayara:hodayara@giving-hands.cztzd.mongodb.net/helpHend?retryWrites=true&w=majority" 
-
+//var url ="mongodb+srv://hodayara:hodayara@giving-hands.cztzd.mongodb.net/helpHend?retryWrites=true&w=majority" 
+var url ="mongodb+srv://hodayara:hodayara@giving-hands.cztzd.mongodb.net/helpHend";
 
 
  router.post('/addNewUser',async function(req, res,next) {
